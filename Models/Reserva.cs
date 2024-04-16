dff410dfd6e0bda0be51b17b6f23b117fdbbfa96
devdouglasa <douglasalisson27@gmail.com>
2024-04-15 21:08:54 -0300

    Implementando métodos da classe Reserva

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // IMPLEMENTADO
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // IMPLEMENTADO
+                throw new ArgumentException("O número de Hospedes é superior à capacidade da Suíte!");
             }
         }
 
@@ -35,23 +34,23 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // IMPLEMENTADO
+            int quantidadeHospedes = Hospedes.Count;
+            return quantidadeHospedes;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
+            // IMPLEMENTADO
             decimal valor = 0;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            valor = DiasReservados * Suite.ValorDiaria;
+
+            // IMPLEMENTADO
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = (valor * 10 ) / 100;
+                valor -= desconto;
             }
 
             return valor;
